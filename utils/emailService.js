19b41a8162973123297570a3a3a009eefd5f328a
Muhammad Irsyad <irsyad0906@gmail.com>
2025-10-17 00:41:18 +0700

    #

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 
 // Konfigurasi transporter untuk Nodemailer menggunakan kredensial dari .env
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com', // Host SMTP untuk Gmail
+    port: 465,              // Port SSL (lebih sering diizinkan)
+    secure: true,           // Menggunakan koneksi SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
