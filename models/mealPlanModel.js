5c2247ea95912b8858226e825e25828216e1be72
Muhammad Irsyad <irsyad0906@gmail.com>
2025-10-30 23:43:09 +0700

    fix: correct food field type in MealPlan schema to ensure proper reference to Food model

--- models/mealPlanModel.js
+++ models/mealPlanModel.js
@@ -4,7 +4,7 @@ const MealPlanSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     date: { type: String, required: true, index: true },
     mealType: { type: String, required: true },
-    food: { type: Object, required: true },
+    food: { type: mongoose.Schema.Types.ObjectId, ref: 'Food', required: true },
     time: String,
 });
 
