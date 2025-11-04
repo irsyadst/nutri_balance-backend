a8a2b2398f0a34a870ad8bec5ee91234ff016172
Muhammad Irsyad <irsyad0906@gmail.com>
2025-11-04 17:04:01 +0700

    feat: refactor user routes to include getNotifications in userController import

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile, updateProfile } = require('../controllers/userController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
 const { getProfile, updateProfile, getNotifications } = require('../controllers/userController');
-
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
 router.get('/profile', authenticateToken, getProfile);
 router.put('/profile', authenticateToken, updateProfile);
