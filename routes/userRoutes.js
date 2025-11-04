185027c40b35022b56471faa799d92a076073229
Muhammad Irsyad <irsyad0906@gmail.com>
2025-11-04 17:01:43 +0700

    feat: add notifications route; include getNotifications in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getProfile, updateProfile } = require('../controllers/userController');
 const { authenticateToken } = require('../middlewares/authMiddleware');
+const { getProfile, updateProfile, getNotifications } = require('../controllers/userController');
+
 
 router.get('/profile', authenticateToken, getProfile);
 router.put('/profile', authenticateToken, updateProfile);
